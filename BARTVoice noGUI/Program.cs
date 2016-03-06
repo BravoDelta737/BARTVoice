37718a2a6b5d81c341550ba7d2fd32838baf0963
BravoDelta737 <jakelanc950@gmail.com>
2016-03-05 22:02:04 -0600

    changed xml reading to target specific elements and added speech

--- BARTVoice noGUI/Program.cs
+++ BARTVoice noGUI/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
 using System.Speech;
+using System.Speech.Synthesis;
 
 namespace BARTVoice_noGUI
 {
@@ -13,33 +14,47 @@ namespace BARTVoice_noGUI
         static void Main(string[] args)
         {
             String URLString = "http://api.bart.gov/api/etd.aspx?cmd=etd&orig=RICH&key=MW9S-E7SL-26DU-VV8V";
+            String destination;
+            String t2a;
+            String length;
             XmlTextReader reader = new XmlTextReader(URLString);
-            
-
-
-
-            for (int i = 0; i <= 6; i++)
+            SpeechSynthesizer synth = new SpeechSynthesizer();
+            while (true)
             {
-                Console.WriteLine("TACOS");
+                reader.ReadToFollowing("etd");
                 reader.Read();
-
-                switch (reader.NodeType)
+                reader.Read();
+                destination = reader.Value;
+                reader.ReadToFollowing("minutes");
+                reader.Read();
+                t2a = reader.Value;
+                reader.ReadToFollowing("length");
+                reader.Read();
+                length = reader.Value;
+                synth.Speak(length + " car train for " + destination + " in " + t2a + "minutes");
+                Console.ReadKey();
+            }
+           /* while (reader.Read()) {
                 {
-                    case XmlNodeType.Element: // The node is an element.
-                        Console.Write("<" + reader.Name);
-                        Console.WriteLine(">");
-                        break;
+                   
 
-                    case XmlNodeType.Text: //Display the text in each element.
-                        Console.WriteLine(reader.Value);
-                        break;
+                    switch (reader.NodeType)
+                    {
+                        case XmlNodeType.Element: // The node is an element.
+                            Console.Write("<" + reader.Name);
+                            Console.WriteLine(">");
+                            break;
 
-                    case XmlNodeType.EndElement: //Display the end of the element.
-                        Console.Write("</" + reader.Name);
-                        Console.WriteLine(">");
-                        break;
-                }
+                        case XmlNodeType.Text: //Display the text in each element.
+                            Console.WriteLine(reader.Value);
+                            break;
 
+                        case XmlNodeType.EndElement: //Display the end of the element.
+                            Console.Write("</" + reader.Name);
+                            Console.WriteLine(">");
+                            break;
+                    }
+                }*/
 
                 Console.ReadKey();
 
@@ -47,5 +62,4 @@ namespace BARTVoice_noGUI
 
         }
     }
-}
 
